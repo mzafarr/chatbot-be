10b1acb8c970a9ea7efaa3e9fca9b4d40abe936f
Muhammad Zafar <mzafar611@gmail.com>
2023-09-23 09:40:01 +0500

    commit

--- app/structured_output/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ChatWindow } from "@/components/ChatWindow";
-
-export default function AgentsPage() {
-  return (
-    <ChatWindow
-      endpoint="api/chat/structured_output"
-    ></ChatWindow>
-  );
-}
